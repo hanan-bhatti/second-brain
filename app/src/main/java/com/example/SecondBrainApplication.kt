c4d92d2147f47db9adfe5429194ab85c4f4b13b9
hanan-bhatti <hannanbhatti2006@gmail.com>
2026-07-19 16:05:52 +0500

    feat: register VideoFrameDecoder in Coil ImageLoader

--- app/src/main/java/com/example/SecondBrainApplication.kt
+++ app/src/main/java/com/example/SecondBrainApplication.kt
@@ -6,11 +6,23 @@
 package com.example
 
 import android.app.Application
+import coil.ImageLoader
+import coil.ImageLoaderFactory
+import coil.decode.VideoFrameDecoder
 import com.google.firebase.FirebaseApp
 
-class SecondBrainApplication : Application() {
+class SecondBrainApplication : Application(), ImageLoaderFactory {
     override fun onCreate() {
         super.onCreate()
         FirebaseApp.initializeApp(this)
+    }
+
+    override fun newImageLoader(): ImageLoader {
+        return ImageLoader.Builder(this)
+            .components {
+                add(VideoFrameDecoder.Factory())
+            }
+            .crossfade(true)
+            .build()
     }
 }
