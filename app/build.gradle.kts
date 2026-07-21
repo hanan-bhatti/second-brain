5c88397d5ac98ed4b67ae6f19b99cfb0ff49fc8d
hanan-bhatti <hannanbhatti2006@gmail.com>
2026-07-21 07:21:42 +0500

    Bump version to 0.9.1-beta02

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
     applicationId = "com.hanan_bhatti.second_brain"
     minSdk = 24
     targetSdk = 37
-    versionCode = 3
-    versionName = "0.9.1-beta01"
+    versionCode = 4
+    versionName = "0.9.1-beta02"
 
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
   }
